01ffe525d25702256840e04a208c3c9fb106b430
jakedempsey <jakedempsey88@gmail.com>
2022-07-22 23:46:53 +1000

    got http requests working

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 50000;
-const cors = require('cors');
-
-app.use('/', (req, res, next) => {
-    console.log("Base path...");
-    next();
-})
-app.use('/hey', (req, res, next) => {
-    console.log("the hey path!");
-    res.json({you: "found the hey path!"});
-})
+const cors = require("cors");
+app.use(cors()); //required, else requests will be blocked from react
+app.use("/", (req, res, next) => {
+  console.log("Base path...");
+  next();
+});
+app.use("/hey", (req, res, next) => {
+  console.log(req?.query);
+  res.json({ you: "found the hey path!" });
+});
 app.listen(process.env.PORT || 50000, () => {
-    console.log("Checkers server has started...")
-})
+  console.log("Checkers server has started...");
+});
