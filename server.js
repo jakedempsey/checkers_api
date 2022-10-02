10dde45002f3b2d07d967b7f07eb6960d94cbf69
jakedempsey <jakedempsey88@gmail.com>
2022-10-02 20:09:59 +1100

    updated some stuff

--- server.js
+++ server.js
@@ -2,15 +2,47 @@ const express = require("express");
 const app = express();
 const port = 50000;
 const cors = require("cors");
-app.use(cors()); //required, else requests will be blocked from react
+const config = require("./config");
+app.use(cors()); //required, or requests will be blocked from react
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const url = config.MONGO_URL;
+const jwt = require("jsonwebtoken");
+//app.use(express.json());
+
 app.use("/", (req, res, next) => {
-  console.log("Base path...");
+  console.log("---Base path---");
   next();
 });
-app.use("/hey", (req, res, next) => {
-  console.log(req?.query);
-  res.json({ you: "found the hey path!" });
+//Latest node update means you have to specifically add a body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post("/message", async (req, res) => {
+  const db = await MongoClient.connect(url);
+  let dbo = db.db("admin");
+  const result = await dbo.collection("messages").insertOne(req.body);
+  res.json({ successful: result });
+});
+app.get("/getMessage", async (req, res) => {
+  const db = await MongoClient.connect(url);
+  let dbo = db.db("admin");
+  const result = await dbo
+    .collection("messages")
+    .findOne({ message: "message1" });
+  res.json(result);
+});
+app.put("/putMessage", async (req, res) => {
+  const db = await MongoClient.connect(url);
+  let dbo = db.db("admin");
+  await dbo
+    .collection("messages")
+    .updateOne(
+      { message: "message1" },
+      { $set: { updated: req.body.message } }
+    );
+  res.end();
+  return;
 });
 app.listen(process.env.PORT || 50000, () => {
-  console.log("Checkers server has started...");
+  console.log("---Starting Checkers API---");
 });
